d7e0d39bb437e18e941ebe4b899a0884220c3107
gentacle <gentacle00@gmail.com>
2019-04-29 18:31:12 +0900

    add @babel/plugin-proposal-class-properties

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -11,6 +11,18 @@ module.exports={
     entry:{
         app: ['./client.jsx'], //'./WordRelay.jsx'를 client.jsx에서 불러옴.필요없음.
     },//입력
+
+    module: {
+        rules:[{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options:{
+                presets:['@babel/preset-env','@babel/preset-react'],
+                plugins:['@babel/plugin-proposal-class-properties'],
+            },
+        }],
+    },
+
     output:{
         path: path.join(__dirname, 'dist'), //C:\workspace\javascript\lecture\dist
         filename: 'app.js'
