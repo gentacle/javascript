c4562e12e15aed2cf3bae534fb1b2abce74dbf9b
gentacle <gentacle00@gmail.com>
2019-05-11 15:15:19 +0900

    start 'NumberBaseball'

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,9 +1,9 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const {hot} = require('react-hot-loader/root')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {hot} from 'react-hot-loader/root';
 
-const WordRelay = require('./wordRelay');
+import NumberBaseball from './NumberBaseball';
 
-const Hot = hot(WordRelay);
+const Hot = hot(NumberBaseball);
 
 ReactDOM.render(<Hot />, document.querySelector('#root'));
