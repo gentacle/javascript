9ebdd116da2cf0da3f5db25daaef6fbaa46d4290
gentacle <gentacle00@gmail.com>
2019-05-11 19:41:17 +0900

    use {props}

--- lecture/Try.jsx
+++ lecture/Try.jsx
@@ -1,15 +1,12 @@
 import React,{Component} from 'react';
 
-class Try extends Component{
-  render(){
-    const tryInfo = this.props.tryInfo;
-    return(
+const Try = ({ tryInfo })=>{
+  return(
       <li>
         <div> {tryInfo.try} </div>
         <div> {tryInfo.result} </div>
       </li>
-    )
-  }
+  )
 }
 
 export default Try;
