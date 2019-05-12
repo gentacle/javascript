b02e9d0a8eb79c8db19382b2f842d2c1f9b186d2
gentacle <gentacle00@gmail.com>
2019-05-12 16:00:37 +0900

    'update' : use PureComponent

--- react-webgame/숫자야구/Try.jsx
+++ react-webgame/숫자야구/Try.jsx
@@ -1,12 +1,15 @@
-import React,{Component} from 'react';
+import React,{PureComponent} from 'react';
 
-const Try = ({ tryInfo })=>{
-  return(
-      <li>
-        <div> {tryInfo.try} </div>
-        <div> {tryInfo.result} </div>
-      </li>
-  )
+class Try extends PureComponent{
+  render(){
+    const {tryInfo} = this.props;
+      return(
+        <li>
+          <div> {tryInfo.try} </div>
+          <div> {tryInfo.result} </div>
+        </li>
+      );
+  }
 }
 
 export default Try;
