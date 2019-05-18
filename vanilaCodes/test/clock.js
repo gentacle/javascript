7b908ca5b5c3a65e62d2352e33a23489733167e8
gentacle <gentacle00@gmail.com>
2019-05-18 17:02:26 +0900

    update : js-clock

---
+++ vanilaCodes/test/clock.js
@@ -0,0 +1,20 @@
+const clockTag = document.querySelector(".js-clock h1");
+
+function getTime() {
+  const setDate = new Date(),
+    getHour = addPadLeft(setDate.getHours()),
+    getMinute = addPadLeft(setDate.getMinutes()),
+    getSecond = addPadLeft(setDate.getSeconds());
+  clockTag.innerText = `${getHour}:${getMinute}:${getSecond}`;
+}
+
+function addPadLeft(value) {
+  return value < 10 ? "0" + value : value;
+}
+
+function init() {
+  getTime();
+  const countTime = setInterval(getTime, 1000);
+  setTimeout(() => clearInterval(countTime), 3000);
+}
+init();
