19ce3e882a21cfa4c993bf2052bb3cddb9ff15bb
gentacle <gentacle00@gmail.com>
2019-05-19 21:17:02 +0900

    update: bgImage 계속 변화하게 함

--- vanilaCodes/test/background.js
+++ vanilaCodes/test/background.js
@@ -3,18 +3,20 @@ const body = document.querySelector("body");
 const IMG_NUMBER = 3;
 
 
-
-function handleImagLoad(){
-  console.log("finished loading");
-}
-
 //image 형식 객체화 및 설정.
 function paintImage(randomNumber){
-  const image = new Image();
+  let image = new Image();
   image.src = `images/${randomNumber + 1}.jpg`;
   image.classList.add("bgImage");
-  body.appendChild(image);
-  // image.addEventListener("loadend",handleImagLoad)
+  
+  
+  if(body.querySelector("img")){
+    let bgImage = body.querySelector(".bgImage");
+    console.log(bgImage);
+    body.replaceChild(image,bgImage);
+  }else{
+    body.appendChild(image);
+  }
 }
 
 function genRandom(){
@@ -25,6 +27,9 @@ function genRandom(){
 function init(){
   const randomNumber = genRandom();
   paintImage(randomNumber);
+  setInterval(() => {
+    let randomNumber = genRandom();
+    paintImage(randomNumber);
+  }, 10000);
 }
-
 init();
